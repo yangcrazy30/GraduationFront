d0e745de674e20c94e4b5d1fb80f7d2e3a487e5d
Zeo <yangcrazy30@hotmail.com>
2020-03-14 18:15:49 +0800

    update axios config

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import qs from "qs";
-axios.defaults.baseURL = "http://127.0.0.1:3000";
+axios.defaults.baseURL = "http://101.200.209.120:3000";
 axios.defaults.withCredentials = true;
 
-axios.interceptors.request.use(function(config) {
+axios.interceptors.request.use(function (config) {
   config.headers.common["Authorization"] =
     "Bearer " + localStorage.getItem("ujs");
   if (config.method === "get") {
-    config.paramsSerializer = function(params) {
+    config.paramsSerializer = function (params) {
       return qs.stringify(params, { arrayFormat: "repeat" });
     };
   }
